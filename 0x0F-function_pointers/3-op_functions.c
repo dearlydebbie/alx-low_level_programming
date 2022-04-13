97f674980c10de54c00df0022ab72a0096f3310c
Deborah Adeoye <debbieadeoye1@gmail.com>
2022-04-13 18:37:55 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,60 +3,73 @@
 #include <stdio.h>
 
 /**
- * op_add - adds 2 numbers
- * @a: first number to add
- * @b: second number to add
- * Return: sum of the numbers
- */
-
+  * op_add - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - subtracts two numbers
- * @a: first number
- * @b: second number
- * Return: difference of the numbers
- */
+  * op_sub - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - multiplies two numbers
- * @a: first number
- * @b: second number
- * Return: The result to multiply a * b.
- */
-
+  * op_mul - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - divides two numbers
- * @a: first number
- * @b: second number
- * Return: division of a by b
- */
-
+  * op_div - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a / b);
 }
 
 /**
- * op_mod - divides two numbers, returns the remainder
- * @a: first number
- * @b: second number
- * Return: remainder of the division
- */
-
+  * op_mod - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a % b);
 }
