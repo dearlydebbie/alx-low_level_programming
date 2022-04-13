fd59ba9a2b409007128cd4f9a5eeac9fa60865d3
Deborah Adeoye <debbieadeoye1@gmail.com>
2022-04-13 18:48:25 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,30 @@
 #include "3-calc.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
-  * get_op_func - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
-int (*get_op_func(char *s))(int, int)
+ * main - program to perform simple operations
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: exits 0 normally, 98 on argc error, 99 on illegal operator
+ */
+
+int main(int argc, char *argv[])
 {
-	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div },
-	{ "%", op_mod },
-	{ NULL, NULL }
-	};
-	int i = 0;
+	int (*func)(int, int);
 
-	while (i < 5)
+	if (argc != 4)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-
-		i++;
+		printf("Error\n");
+		exit(98);
 	}
-
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
